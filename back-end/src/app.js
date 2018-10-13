37d6237bdf53ab2175e170d7744f6542f3fd17df
cjh724 <jh.cho133104@gmail.com>
2018-10-13 23:36:32 +0900

    close #23 - test2

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -10,12 +10,20 @@ app.get('/author', function(res,req){
         age: "25"
     }
     res.json(result);
-})
+});
 
 app.get('/blist', function(res,req) {
     const result = {
         author: "안녕",
         age: 1
+    }
+    res.json(result);
+});
+
+app.get('/test', function(res, req) {
+    const result = {
+        id : 1,
+        text : "테스트중입니다"
     }
     res.json(result);
 });
