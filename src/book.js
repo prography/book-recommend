28281896e5b5c156ccfe71179a23dcdf11cae4ae
Q00 <jqyu.lee@gmail.com>
2018-10-15 03:23:03 +0900

    fix #28 fix internal server error in aws

--- src/book.js
+++ src/book.js
@@ -1,14 +1,11 @@
+'use strict';
 const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 app.use(express.urlencoded({extended: false}));
 
-
 app.get('/', (req, res) => {
-    res.json({"hello serverless":true});
+    res.json({"hello serverless book":true});
 });
 
-app.listen(3000,() => {
-    console.log('listen 3000');
-});
-module.exports.handler = serverless(app);
+export const book = serverless(app);
