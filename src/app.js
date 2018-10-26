7379b3d34bd08e7ca2c5c0168e647c5a9deb58ca
Q00 <jqyu.lee@gmail.com>
2018-10-26 11:13:57 +0900

    add server.js serverless.js to seperate local and server

---
+++ src/app.js
@@ -0,0 +1,17 @@
+'use strict';
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+    const boooook = {
+	book: "hi",
+	author: "lee"
+    };
+    res.json(boooook);
+    
+});
+
+export default app;
