a4132e2c716d7ca6254a2870bc6f02219f3f76c6
suev7245 <suev72@naver.com>
2019-01-11 23:28:28 +0900

    (refactor) refactoring get refresh token

--- src/lib/middleware/authCheck.js
+++ src/lib/middleware/authCheck.js
@@ -6,14 +6,19 @@ import { privateEncrypt } from 'crypto';
 import jwt from 'jsonwebtoken'
 import jwkToPem from 'jwk-to-pem'
 import * as AmazonCognitoIdentity from 'amazon-cognito-identity-js';
+const CognitoIdToken = require('amazon-cognito-identity-js-node').CognitoIdToken;
+const CognitoAccessToken = require('amazon-cognito-identity-js-node').CognitoAccessToken;
+const CognitoRefreshToken = require('amazon-cognito-identity-js-node').CognitoRefreshToken;
 
 export default() => (req,res,next) => {
-
+    console.log('here')
     //토큰 받아오기
     const access = req.body.accessToken
     //console.log(access)
+
+    const refresh = new CognitoRefreshToken({RefreshToken: req.body.refreshToken});
     const id = req.body.idToken
-    const refresh = req.body.refreshToken
+    //const refresh = req.body.refreshToken
     //https://github.com/awslabs/aws-support-tools/tree/master/Cognito/decode-verify-jwt 
     //jwk 만들기
     const jwk = config.jwk  
@@ -28,21 +33,27 @@ export default() => (req,res,next) => {
     console.log(decode_token)
 
     //time expired
-    if(decode_token.auth_time > new Date()){
+    if(decode_token.auth_time < new Date()){
         console.log('good')
-        if(decode_token.auth_time - new Data() < 5*60*1000){
+        if(decode_token.auth_time - new Date() < 5*60*1000){
             //refresh token
-            cognitoUser = getCognitoUser(decode_token)	    	     
+            const cognitoUser = getCognitoUser(decode_token)
+            // Authenticate user
+            console.log(cognitoUser)
+
             cognitoUser.refreshSession(refresh, (err, session) => {
+ 
                 if ( err ) return res.setStatus(404)
-                AWS.config.credentials = getCognitoIdentityCredentials(id)
+                const tokens = getTokens(session)
+                AWS.config.credentials = getCognitoIdentityCredentials(tokens)
+                console.log(AWS.config.credentials)
                 AWS.config.credentials.get(function(){
                     const credentials = AWS.config.credentials.data.Credentials
                     //token 어떻게 들어오는지 확인 필요
                     req.body.sessionToken = credentials.SessionToken
                     return next()
-                })
-	        })
+                })  
+            })
 	    }
     }else{
         console.log('expired')
@@ -65,24 +76,33 @@ export default() => (req,res,next) => {
 
     return next()
 }
-
+const getTokens = function(session) {
+    return {
+      accessToken: session.getAccessToken().getJwtToken(),
+      idToken: session.getIdToken().getJwtToken(),
+      refreshToken: session.getRefreshToken().getToken()
+    };
+  };
 
 const getCognitoUser = (token) => {
     const poolData = {
 	UserPoolId :config.poolData.UserPoolId,
-	ClientId : config.poolDaata.ClientId
+	ClientId : config.poolData.ClientId
     };
     const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+    console.log(userPool)
+    console.log(token)
     const userData = {
-	Username : token.email,
+	Username : token['cognito:username'],
 	Pool: userPool
     }
+    console.log(userData)
     return new AmazonCognitoIdentity.CognitoUser(userData)
 }
 
 const getCognitoIdentityCredentials = (token) => {
     const loginInfo = {};
-    loginInfo[`cognito-idp.${config.pool_region}.amazonaws.com/${config.poolData.UserPoolId}`] = token
+    loginInfo[`cognito-idp.${config.pool_region}.amazonaws.com/${config.poolData.UserPoolId}`] = token.idToken
     const params = {
 	IdentityPoolId: config.poolData.IdentityPoolId,
 	Logins: loginInfo
