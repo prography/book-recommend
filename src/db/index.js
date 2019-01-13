5362c830c5e88a3f5fcbfb84a675ae3a2fb1e55a
Q00 <jqyu.lee@gmail.com>
2019-01-13 23:45:15 +0900

    (feat) add singletone pattern and sample sql

---
+++ src/db/index.js
@@ -0,0 +1,24 @@
+import mysql from 'mysql'
+
+class singleton {
+    static instance;
+    constructor(){
+	if (instance) return instance;
+
+	instance = mysql.createPool({
+	    connectionLimit : 10,
+	    host : config.db.host,
+	    user : config.db.user,
+	    password: config.db.pw,
+	    database: config.db.database
+
+	})
+	pool.on('enqueue', function(){
+	    console.log('waiting for availiable connection pool')
+	}
+    }
+}
+
+
+module.exports= singleton
+
