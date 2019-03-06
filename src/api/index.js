d98a613c1fd9d303a0dac2b559714aefbfa52c98
Q00 <jqyu.lee@gmail.com>
2019-03-07 00:16:18 +0900

    (refactor) delete authcheck user api

--- src/api/index.js
+++ src/api/index.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.use('/auth', auth);
 router.use('/auth2',authCheck(), auth);
-router.use('/user',authCheck(), user);
+router.use('/user',user);
 router.use('/books', book);
 router.use('/tags', tag);
 
